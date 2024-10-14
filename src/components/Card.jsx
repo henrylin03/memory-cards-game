9db9fdae57d51417c9c990d20fe1ab3b1eec61fb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-10-14 14:51:07 +1100

    feat: pokemons are randomly generated by calling the API using the Pokemon's unique ID. ensure no duplicates in Pokemons being displayed. the list of Pokemon IDs are generated using helper functions at the Gameboard component level, with each ID passed into the Card component (child of Gameboard). the Card component then uses the useEffect hook to call the API to get the name and sprite image of the Pokemon to be displayed in the Card.

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import "../styles/card.css";
+
+export default function Card({ id }) {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonImageUrl, setPokemonImageUrl] = useState("");
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((results) => results.json())
+      .then((data) => {
+        setPokemonName(data.name);
+        setPokemonImageUrl(data.sprites.front_default);
+      });
+  }, [id]);
+
+  return (
+    <button className="card">
+      <figure>
+        <img src={pokemonImageUrl} alt={`image of ${pokemonName}`} />
+      </figure>
+      <p>{pokemonName}</p>
+    </button>
+  );
+}
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,32 +1,27 @@
+import { useState } from "react";
 import "../styles/gameboard.css";
+import Card from "./Card";
 
-function Card() {
-  return (
-    <button className="card">
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-          alt="image of Ditto"
-        />
-      </figure>
-      <p>Ditto</p>
-    </button>
-  );
-}
+const getRandomId = (maxId = 150) => Math.floor(Math.random() * maxId + 1);
 
 export default function Gameboard() {
-  return (
-    <section className="gameboard">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </section>
-  );
+  const [pokemonIds, setPokemonIds] = useState(getRandomPokemonIds());
+
+  function getRandomPokemonIds() {
+    let ids = new Set();
+
+    for (let i = 0; i < 10; i++) {
+      let randomId = getRandomId();
+      while (ids.has(randomId)) randomId = getRandomId();
+      ids.add(randomId);
+    }
+
+    return [...ids];
+  }
+
+  const gameCards = pokemonIds.map((pokemonId) => (
+    <Card key={pokemonId} id={pokemonId} />
+  ));
+
+  return <section className="gameboard">{gameCards}</section>;
 }
