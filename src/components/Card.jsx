356dab9c4bbad5fee4418d92600a504d242dbfe9
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-10-19 14:25:26 +1100

    feat: player cannot click a card if it is still loading. fixes #10.
    disabled state has been styled to not have the size increase and the yellow aura box-shadow. users shouldn't be able to click a card if a pokemon hasn't been fetched yet

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,23 @@ import "../styles/card.css";
 export default function Card({ id, handleCardSelection }) {
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonImageUrl, setPokemonImageUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((results) => results.json())
       .then((data) => {
         setPokemonName(data.name);
         setPokemonImageUrl(data.sprites.front_default);
+        setIsLoading(false);
       });
   }, [id]);
 
+  const handleMouseDown = () => handleCardSelection(id);
+
   return (
-    <button className="card" onMouseDown={() => handleCardSelection(id)}>
+    <button className="card" onMouseDown={handleMouseDown} disabled={isLoading}>
       <figure>
         <img src={pokemonImageUrl} alt={`image of ${pokemonName}`} />
       </figure>
