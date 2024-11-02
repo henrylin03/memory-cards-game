330796eb40e83fa7562a441c2b98c4478b8b6b16
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-11-02 11:47:51 +1100

    refactor: use custom hook to fetch data. fix: prevent double-clicking on card leading to loss. refactor: add props type checking for Card component. (#21)
    1. fixes #17 by introducing custom hook, `useAllPokemon`, that returns
    the data, error, and loading state as a boolean value. this hook handles
    all logic now with fetching from PokeAPI.
    2. fixes #10 tangentially. we don't want users to double click on a card
    by accident and then instantly lose the game. we just hide the cards
    during loading state.
    3. add prop type checking for `<Card />` component to start. this kicks
    off #20.

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,48 +1,31 @@
-import { useEffect, useState } from "react";
-import Card from "../Card";
-import LoadingScreen from "../LoadingScreen";
+import { useState } from "react";
+import Card from "./Card";
+import LoadingScreen from "./LoadingScreen";
+import useAllPokemon from "../hooks/useAllPokemon";
 import {
   getRandomId,
   getRandomPokemonIds,
   shuffleElements,
   randomlySelectElements,
-} from "../../helpers";
-import fetchPokemonData from "./fetchPokemonData";
-import "../../styles/gameboard.css";
+} from "../helpers";
+import "../styles/gameboard.css";
+
+const TOTAL_IDS = 10;
 
 export default function Gameboard({ incrementScore, resetScore }) {
-  const TOTAL_IDS = 10;
   const [displayedPokemonIds, setDisplayedPokemonIds] = useState(
     getRandomPokemonIds(TOTAL_IDS)
   );
   const [selectedPokemonIds, setSelectedPokemonIds] = useState(new Set());
-  const [displayedPokemonObjects, setDisplayedPokemonObjects] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { pokemons, error, isLoading } = useAllPokemon(displayedPokemonIds);
 
-  useEffect(() => {
-    const fetchAllPokemonData = async (ids) => {
-      try {
-        const promises = ids.map((id) => fetchPokemonData(id));
-        const results = await Promise.all(promises);
-        setDisplayedPokemonObjects(results);
-
-        for (const pokemonObject of results) {
-          await new Promise((resolve) => {
-            const img = new Image();
-            img.src = pokemonObject.imageUrl;
-            img.onload = () => resolve();
-          });
-        }
-
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching all Pokemon data:", error);
-      }
-    };
-
-    fetchAllPokemonData(displayedPokemonIds);
-    return () => setIsLoading(true);
-  }, [displayedPokemonIds]);
+  //TODO: handle error
+  if (isLoading)
+    return (
+      <section className="gameboard">
+        <LoadingScreen />
+      </section>
+    );
 
   const regeneratePokemonIds = (selectedPokemonId) => {
     const MAX_IDS_OF_POKEMONS_ALREADY_SELECTED = 5;
@@ -98,17 +81,16 @@ export default function Gameboard({ incrementScore, resetScore }) {
     }
   };
 
-  const gameCards = displayedPokemonObjects.map((pokemon) => (
-    <Card
-      key={pokemon.id}
-      handleCardSelection={handleCardSelection}
-      pokemonData={pokemon}
-    />
-  ));
-
   return (
     <section className="gameboard">
-      {isLoading ? <LoadingScreen /> : gameCards}
+      {pokemons.map((pokemon) => (
+        <Card
+          key={pokemon.id}
+          handleCardSelection={handleCardSelection}
+          pokemonData={pokemon}
+          isLoading={isLoading}
+        />
+      ))}
     </section>
   );
 }
